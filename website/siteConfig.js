996f64f2e5b46a9c36cd7e8976ee6cd0ea855c54
Anne Wagner <44533995+amwagner19@users.noreply.github.com>
2020-10-02 16:10:11 -0700

    Updated tagline

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Anne Wagner', // Title for your website.
-    tagline: 'Aspiring technologist interested in Software Development, Data Science and Engineering, and Cybersecurity.',
+    tagline: 'aspiring technologist',
     profilePhoto: 'img/profile.png',
     url: 'https://amwagner19.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
